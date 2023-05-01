d03d220898be0ab92ae9abb0dc319a890cea42f5
Ben Walsh <benjaminwalsh@hotmail.co.uk>
2023-05-01 21:19:34 +0100

    add side nav vue support and customise some of the nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+import react from "@astrojs/react";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [tailwind(), image(), react(), vue()],
   experimental: {
     assets: true
-   }
+  }
 });
-

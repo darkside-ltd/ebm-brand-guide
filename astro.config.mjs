a6c1ecaa5020a06bb07f3a2ea18c08c67c63cd72
Ben Walsh <benjaminwalsh@hotmail.co.uk>
2023-05-04 20:12:30 +0100

    try polyfill for vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,21 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
-
 import mdx from "@astrojs/mdx";
+import legacy from '@vitejs/plugin-legacy';
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image(), react(), vue(), mdx()],
   experimental: {
     assets: true
-  }
+  },
+  vite: {
+    plugins: [
+      legacy({
+        targets: ['defaults', 'not IE 11'],
+      }),
+    ],
+  },
 });
